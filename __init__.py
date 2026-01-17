743936ee226014fcb5dd6aa6cf014b021e9b67fb
BaxtersLab <baxtersbeats@outlook.com>
2026-01-16 21:01:10 -0800

    Fix import to use relative import for ComfyUI compatibility

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 ComfyUI Synth Mimic Glitcher - Pitch-following synthesizer with glitch effects
 """
 
-from node3_glitcher import ComfySynthMimicGlitcher
+from .node3_glitcher import ComfySynthMimicGlitcher
 
 NODE_CLASS_MAPPINGS = {
     "ComfySynthMimicGlitcher": ComfySynthMimicGlitcher,
